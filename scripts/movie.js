abd105de4420fb21f52b6db57c9e562d11c320cb
Nwachukwu Ogbonnaya Prince <princenwachukwu308@yahoo.com>
2024-06-01 13:58:03 +0100

    genre

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,9 +1,11 @@
+////// API KEK : d4beeb5a4amshafb9404318aea55p160adcjsnd5b90d233e8e ///////////////
+
 // Mobile view port Menu////////////////////////////////////
 const humbug = document.querySelector(".hambug");
 const humbugUl = document.querySelector(".hambug-ul");
 
 // Light Mode ajuster////////////////////////////////////////
-const lightMode = document.querySelector(".fa-adjust");
+const lightMode = document.querySelectorAll(".fa-adjust");
 const webBody = document.querySelector(".bd");
 
 // Input for search /////////////////////////////////////////
@@ -38,13 +40,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
     
 })
+lightMode.forEach(modes => {
 
-lightMode.addEventListener("click", ()=>{  
+    modes.addEventListener("click", ()=>{  
 
-    webBody.classList.toggle("light-body");  
+        webBody.classList.toggle("light-body");  
+    
+        localStorage.setItem("lightMode", webBody.className);
+    })
+});
 
-    localStorage.setItem("lightMode", webBody.className);
-})
 
 // Input for search /////////////////////////////////////////
 formSrc.forEach(form => {
@@ -65,7 +70,7 @@ async function tvMaizeData(searchInputRequest){
     try {
         const req = await fetch(`https://api.tvmaze.com/search/shows?q=${searchInputRequest}`);
 
-        if(!req.ok){
+        if(!req.ok || req == 400){
             alert("Error connecting!! \nPlease Check your internet connection");
         }
         const response = await req.json();
@@ -131,27 +136,59 @@ function moviesView(data){
         similarMovies.appendChild(simImg);
         
     }
+}
 
-    // const axios = require('axios');
+// Genre part ////////////////////////////////////////
+const homeGenre = document.querySelector(".genres");
 
-    // const options = {
-    // method: 'GET',
-    // url: 'https://streaming-availability.p.rapidapi.com/genres',
-    // params: {
-    //     output_language: 'en'
-    // },
-    // headers: {
-    //     'X-RapidAPI-Key': 'a8260f6512msh5aa686924dfe3fdp15bd85jsn6bfaac25d8fe',
-    //     'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-    // }
-    // };
+const url = 'https://imdb8.p.rapidapi.com/title/list-popular-genres';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a8260f6512msh5aa686924dfe3fdp15bd85jsn6bfaac25d8fe',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+};
 
-    // try {
-    //     const response = await axios.request(options);
-    //     console.log(response.data);
-    // } catch (error) {
-    //     console.error(error);
-    // }
+async function genrHome(){
+
+    try {
+        const response = await fetch(url, options);
+        if(!response.ok){
+            alert("Error in network connection!!!");
+        }
+
+        const result = await response.json();
+        console.table(result);
+        genHomeData(result);
+
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+
+genrHome();
+
+function genHomeData(gens){
+
+    const gensDatasUnlocked = gens.genres;
+ 
+    for(let genName of gensDatasUnlocked){
+
+        let genLi = document.createElement("li");
+        genLi.textContent = genName.description; 
+        
+        genLi.addEventListener("click", ()=>{
+            
+            genLi.style.fontWeight = "bolder";
+     
+        })
+
+        homeGenre.appendChild(genLi);
+        console.log(genLi);
+    }
 
 }
+
 
