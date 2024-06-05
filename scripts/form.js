85589806d178daa9831ad728a68b9ab90c78c7bf
Nwachukwu Ogbonnaya Prince <princenwachukwu308@yahoo.com>
2024-06-05 16:12:54 +0100

    join js

--- scripts/form.js
+++ scripts/form.js
@@ -1,6 +1,6 @@
 const form = document.querySelector(".regForm");
 
-form.addEventListener("submit", (e)=>{
+form.addEventListener("submit", async(e)=>{
 
     e.preventDefault();
 
@@ -9,5 +9,17 @@ form.addEventListener("submit", (e)=>{
     for(let[name, value] of fromData){
 
         console.log(name, value);
+    }
+
+    try {
+
+        const dataReq = await fetch("#url", {method: "POST", body: fromData});
+        if (!dataReq.ok){alert("Error in connection!")};
+
+        const response = await dataReq.json();
+        console.log(response);
+        
+    } catch (error) {
+        
     }
 })
